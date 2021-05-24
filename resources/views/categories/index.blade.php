836739be72ca6fe66e215a02595643fe6bc619ba
unknown <hon_da-1998@hotmail.com>
2021-05-24 18:13:48 +0700

    Show Tabular Data

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -3,4 +3,28 @@
     <div class="d-flex justify-content-end mb-2">
         <a href="/categories/create" class="btn btn-success">Add Category</a>
     </div>
+    <div class="card-default">
+        <div class="card-header">
+            Category
+        </div>
+        <div class="card-body">
+            <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                        <th>Name</th>
+    
+                    </tr>
+                </thead>
+    
+                @foreach($categories as $category)
+                <tbody>
+                <tr>
+                    <td>{{ $category->name }}</td>
+
+                </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
