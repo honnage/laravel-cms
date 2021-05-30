b6695e3aecaa3f6b81c40f7d51f6182a2d0f8718
unknown <hon_da-1998@hotmail.com>
2021-05-31 02:43:43 +0700

    case of unable to find information on the article

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,17 +41,20 @@
 
             <div class="col-md-8 col-xl-9">
               <div class="row gap-y">
-                @foreach($posts as $post)
+                @forelse($posts as $post)
                 <div class="col-md-6">
-                    <div class="card border hover-shadow-6 mb-6 d-block">
-                      <a href="blog/posts/{{$post->id}}"><img class="card-img-top" src="storage/{{$post->image}}" alt="Card image cap" ></a>
-                      <div class="p-6 text-center">
-                        <p><a class="small-5 text-lighter text-uppercase ls-2 fw-400" href="blog/posts/{{$post->id}}">{{$post->category->name}}</a></p>
-                        <h5 class="mb-0"><a class="text-dark" href="#">{{$post->title}}</a></h5>
-                      </div>
+                  <div class="card border hover-shadow-6 mb-6 d-block">
+                    <a href="blog/posts/{{$post->id}}"><img class="card-img-top" src="storage/{{$post->image}}" alt="Card image cap" ></a>
+                    <div class="p-6 text-center">
+                      <p><a class="small-5 text-lighter text-uppercase ls-2 fw-400" href="blog/posts/{{$post->id}}">{{$post->category->name}}</a></p>
+                      <h5 class="mb-0"><a class="text-dark" href="#">{{$post->title}}</a></h5>
                     </div>
                   </div>
-                @endforeach
+                </div>
+                @empty
+                <p class="text-center">No Result : <strong>{{request()->query('search')}}</strong></p>
+                @endforelse
+
               </div>
               {{$posts->appends(['search'=>request()->query('search')])->links()}}
             </div>
