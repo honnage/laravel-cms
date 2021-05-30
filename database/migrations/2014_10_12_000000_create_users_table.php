2e7a90bce71313db167ba339d110282f924c58dd
unknown <hon_da-1998@hotmail.com>
2021-05-30 23:21:55 +0700

    Divide of Admin and Writer status

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('firstname');
             $table->string('lastname');
             $table->string('email')->unique();
+            $table->enum('role',['writer','admin'])->default('writer');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
