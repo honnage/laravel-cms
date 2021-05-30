6ad55fc89ba7f216c86f1a2cae7181d0bf725f30
unknown <hon_da-1998@hotmail.com>
2021-05-31 02:22:22 +0700

    Making a comment system

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -52,6 +52,16 @@
                 @endforeach
             </div>
             
+            <div id="disqus_thread"></div>
+            <script>
+                (function() { // DON'T EDIT BELOW THIS LINE
+                var d = document, s = d.createElement('script');
+                s.src = 'https://http-localhost-7ar1w6unv2.disqus.com/embed.js';
+                s.setAttribute('data-timestamp', +new Date());
+                (d.head || d.body).appendChild(s);
+                })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
           </div>
         </div>
       </div>
